6e1a744d5bf40e9cc176196598ae276491c0eaa4
venkat <devops.bheemireddy@gmail.com>
2024-12-11 14:28:55 +1100

    Roboshop project changes v1.0

--- frontend.sh
+++ frontend.sh
@@ -2,22 +2,17 @@ dnf module disable nginx -y
 dnf module enable nginx:1.24 -y
 dnf install nginx -y
 
-
 systemctl enable nginx
 systemctl start nginx
 
-
 rm -rf /usr/share/nginx/html/*
-
+rm -rf /tmp/frontend.zip
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
-
 
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-
-vim /etc/nginx/nginx.conf
-
+cp nginx.conf /etc/nginx/nginx.conf
 
 systemctl restart nginx
