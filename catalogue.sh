ded9bf97887673e1a9cf48f9c2f7c4e34885d959
venkat <devops.bheemireddy@gmail.com>
2024-12-11 16:03:25 +1100

    Roboshop project changes v1.0

--- catalogue.sh
+++ catalogue.sh
@@ -14,12 +14,14 @@ unzip /tmp/catalogue.zip
 cd /app
 npm install
 
+rm -rf /etc/systemd/system/catalogue.service
 cp catalogue.service /etc/systemd/system/catalogue.service
 
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
 
+rm -rf /etc/yum.repos.d/mongo.repo
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y
 mongosh --host mongodb-dev.azdevopsv82.online </app/db/master-data.js
