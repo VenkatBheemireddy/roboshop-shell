410058464f61827602ec0e349c2d195c9696666b
venkat <devops.bheemireddy@gmail.com>
2024-12-16 19:00:08 +1100

    Roboshop project changes v2.0

--- catalogue.sh
+++ catalogue.sh
@@ -1,28 +1,31 @@
-rm -rf /etc/systemd/system/catalogue.service
-cp catalogue.service /etc/systemd/system/catalogue.service
-
-rm -rf /etc/yum.repos.d/mongo.repo
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-
+# Install NodeJS
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
-
 dnf install nodejs -y
 
-useradd roboshop
 
+#Configure the application
+useradd roboshop
 rm -rf /app
 mkdir /app
+
+# Download the application code
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
 cd /app
 unzip /tmp/catalogue.zip
 
+# Download the dependencies
 cd /app
 npm install
 
-systemctl daemon-reload
+#Setup SystemD Catalogue Service
+cp catalogue.service /etc/systemd/system/catalogue.service
+
+# Start the service
 systemctl enable catalogue
 systemctl start catalogue
 
+# To load schema
+cp /etc/yum.repos.d/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y
-mongosh --host mongodb-dev.azdevopsv82.online </app/db/master-data.js
+mongosh --host MONGODB-SERVER-IPADDRESS </app/db/master-data.js
